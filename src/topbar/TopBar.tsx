8c959effd4a8c5ecae892a8f86da4c582077ce12
Alperen Kılıç <alp.kilic.97@gmail.com>
2023-12-01 09:43:23 +0300

    top bar logo size change

--- src/topbar/TopBar.tsx
+++ src/topbar/TopBar.tsx
@@ -5,7 +5,7 @@ const TopBar = () => {
       <h1 className="uppercase font-studio text-[#1A76BC] hidden lg:block text-3xl xl:text-5xl">
         Kışla Mücadele
       </h1>
-      <img src="/beylikduzu.png" alt="Beylikduzu" className="h-10 sm:h-14 md:h-24" />
+      <img src="/beylikduzu.png" alt="Beylikduzu" className="h-12 sm:h-14 md:h-24" />
     </div>
   );
 };
